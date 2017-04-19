ca68943f6b30462e393c4ae23012cbf21b561237
exodeus <dan.kvelstad@gmail.com>
2017-04-19 17:25:55 +0200

    minor cosmetic stuff

--- sources/testcases/test_presenter.cpp
+++ sources/testcases/test_presenter.cpp
@@ -1,11 +1,7 @@
 #include "gtest/gtest.h"
-#include "presenter\presenter.h"
+#include "presenter/presenter.h"
 
 TEST(skia, hello_world)
 {
 	draw_statemachine();
-	//raster(256, 256, example, "out_raster.png");
-	////ganesh(256, 256, example, "out_ganesh.png");
-	//skpdf(256, 256, example, "out_skpdf.pdf");
-	//picture(256, 256, example, "out_picture.skp");
 }
